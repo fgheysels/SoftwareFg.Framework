f1278817aa87404171a0ff18d559d1b17686d561
frederik.gheysels <frederik.gheysels@telenet.be>
2008-07-05 22:12:39 +0200

    ﻿ [git-vault-id] SoftwareFg_Framework$/SoftwareFg.Framework@33/1393

---
+++ src/SoftwareFg.Framework.EntityObjects/IAuditable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftwareFg.Framework.EntityObjects
+{
+    public interface IAuditable
+    {
+        DateTime Created
+        {
+            get;        
+        }
+
+        DateTime Updated
+        {
+            get;            
+        }
+
+        int Version
+        {
+            get;            
+        }
+
+        void SetCreationDate( DateTime created );
+
+        void SetUpdateDate( DateTime updated );
+
+        void SetVersion( int version );
+
+        string CreatedPropertyName
+        {
+            get;
+        }
+
+        string UpdatedPropertyName
+        {
+            get;
+        }
+    }
+}
