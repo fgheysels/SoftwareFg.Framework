5402c4ac75efe5d40f73bf1bc31bf3c0feee0f97
frederik.gheysels <frederik.gheysels@telenet.be>
2008-07-05 22:12:59 +0200

    ﻿ [git-vault-id] SoftwareFg_Framework$/SoftwareFg.Framework@37/1397

--- src/SoftwareFg.Framework.NHibernateUtils.Tests/SessionManagerTests.cs
+++ src/SoftwareFg.Framework.NHibernateUtils.Tests/SessionManagerTests.cs
@@ -10,28 +10,22 @@ using NUnit.Framework;
 using SoftwareFg.Framework.NHibernateUtils.Tests.TestEntities;
 using NHibernate.Expression;
 using System.IO;
+using SoftwareFg.Framework.NHibernateUtils.Tests.Utils;
 
 namespace SoftwareFg.Framework.NHibernateUtils.Tests
 {
     [TestFixture]
     public class SessionManagerTests
     {
-
-        private string _connectionString;
-
+        
         [TestFixtureSetUp]
         public void CreateTestDatabase()
         {            
             log4net.Config.XmlConfigurator.Configure ();
 
-            // Create an NHibernate Configuration object just to get the connection-string that we're using.
-            Configuration cfg = new Configuration ();
-            cfg.Configure ();
+            Database.CreateTable (this.GetCreatePersonsTableSqlString ());
 
-            _connectionString = cfg.Properties["hibernate.connection.connection_string"].ToString ();
-
-            CreateTables ();
-            
+            Database.Close ();            
         }
 
         [Test]
@@ -81,44 +75,14 @@ namespace SoftwareFg.Framework.NHibernateUtils.Tests
         [TestFixtureTearDown]
         public void DropTestDatabase()
         {
-            SqlConnection conn = new SqlConnection (_connectionString);
 
-            conn.Open ();
-            try
-            {
-                DropTable (conn, "Persons");
-            }
-            finally
-            {
-                conn.Close ();
-            }
+            Database.DropTable ("Persons");
+
+            Database.Close ();            
         }
 
         #region Utility Methods to create Database tables
-
-        private void CreateTables()
-        {
-            SqlConnection conn = new SqlConnection (_connectionString);
-
-            conn.Open ();
-
-            try
-            {
-                CreateTable (conn, this.GetCreatePersonsTableSqlString ());
-            }
-            finally
-            {
-                conn.Close ();
-            }
-        }
-
-        private void CreateTable( SqlConnection conn, string createTableStatement )
-        {
-            SqlCommand cmd = new SqlCommand (createTableStatement, conn);
-
-            cmd.ExecuteNonQuery ();
-        }
-
+        
         private string GetCreatePersonsTableSqlString()
         {
             return "CREATE TABLE Persons ( \n" +
@@ -127,14 +91,7 @@ namespace SoftwareFg.Framework.NHibernateUtils.Tests
                 "[SEX] INT, \n" +
                 "[DateOfBirth] DATETIME )";
         }
-
-        private void DropTable( SqlConnection conn, string tableName )
-        {
-            SqlCommand cmd = new SqlCommand (String.Format ("DROP TABLE {0}", tableName), conn);
-
-            cmd.ExecuteNonQuery ();
-        }
-
+        
         #endregion
 
     }
